7f74f23c562e6f79df340deffebfca120a7cf068
unknown <gmandini@net.hr>
2013-12-15 09:09:02 +0100

    mail validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,6 @@ validates :title, presence: true,
                     length: { minimum: 5 }
 validates :email, confirmation: true,
 					length: { in: 6..50 ,
-					message: "%{value} nije dulji od 6 a manji od 20, odjebi s tim borac!"}
+					message: "%{value} nije dulji od 6 a manji od 20, nista od tog borac!"}
+validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 end
